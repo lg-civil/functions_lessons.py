c89622d55b54c4f74eaf4936c9d39f6d8754bdb0
lg-civil <lgzuniga-urz@cps.edu>
2026-01-28 18:52:10 +0000

    7

--- 7_indefinite_arguments_args.py
+++ 7_indefinite_arguments_args.py
@@ -1,7 +1,54 @@
+# def tea_order(customer_name, tea_type, milk=None, sweetener=None):
+#     print(customer_name, "ordered a ", tea_type, "tea")
+#     if milk !=None:
+#         print("- Add:", milk)
+#     if sweetener != None:
+#         print('- Add:', sweetener)
+
+# tea_order("Alice", "chamomile")
+# tea_order("bob", "black", "oat" )
+# tea_order('tony', 'black', 'oat', 'honey')
+
+# want infinite add-ins
+
+# def tea_order(customer_name, tea_type, *args):
+#     print(customer_name, "ordered a ", tea_type, "tea")
+#     for arg in args:
+#         print('- Add:', arg)
+
+
+# tea_order("Alice", "chamomile")
+# tea_order("bob", "black", "oat" )
+# tea_order('tony', 'black', 'oat', 'honey')
+
+# KeyWord Arguments
+
+
+# def tea_order(customer_name, tea_type, **kwargs):
+#     print(customer_name, "ordered a ", tea_type, "tea")
+#     for key, value in kwargs.items():
+#         print("- Add", key, ':', value)
+
+# tea_order("Alice", "chamomile")
+# tea_order("bob", "black", milk = "oat" )
+# tea_order('tony', 'black', milk ='oat', sweetener = 'honey')
+
+
+
+
+
+
+
+
+
 # Indefinite Arguments (*args) Practice #1
 # Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
 
 # For example for the arguments sum_squares(1,2,3) it should return 14 (1+4+9).
+
+
+
+
 
 
 # Indefinite Arguments (*args) Practice #2
